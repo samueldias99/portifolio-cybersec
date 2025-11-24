e6e1f1d904b626d1687a026454fbbb16a9ef1e7e
samueldias99 <112509036+samueldias99@users.noreply.github.com>
2025-11-23 21:42:11 -0300

    Replace THM icon with shield SVG; update Contact to use official-style icon

--- src/components/icons/TryHackMeIcon.tsx
+++ src/components/icons/TryHackMeIcon.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+// TryHackMe-style shield icon (clean, high-contrast)
 const TryHackMeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" {...props}>
     <defs>
       <linearGradient id="thmGrad" x1="0" x2="1">
-        <stop offset="0%" stopColor="#FF7A18"/>
-        <stop offset="100%" stopColor="#AF002D"/>
+        <stop offset="0%" stopColor="#ff8a00" />
+        <stop offset="100%" stopColor="#ff3d00" />
       </linearGradient>
     </defs>
-    <path d="M12 2L3 6v5c0 5 3.8 9.7 9 11 5.2-1.3 9-6 9-11V6l-9-4z" fill="url(#thmGrad)" />
-    <path d="M8.5 10.5c0-2.485 1.57-4.5 3.5-4.5s3.5 2.015 3.5 4.5c0 2.485-1.57 4.5-3.5 4.5s-3.5-2.015-3.5-4.5z" fill="#fff" opacity="0.06" />
-    <path d="M12 7.5c1.2 0 2 .95 2 2.5s-.8 2.5-2 2.5-2-.95-2-2.5.8-2.5 2-2.5z" fill="#fff" opacity="0.9" />
+    <rect width="512" height="512" rx="96" fill="none" />
+    <path d="M256 40c0 0-136 48-176 88s-40 152-40 152 20 184 216 232c196-48 216-232 216-232s0-112-40-152S256 40 256 40z" fill="url(#thmGrad)" />
+    <g fill="#fff">
+      <path d="M180 220c0-33 27-60 60-60 15 0 29 5 40 14l-18 24c-7-6-16-10-26-10-22 0-40 18-40 40s18 40 40 40c10 0 19-4 26-10l18 24c-11 9-25 14-40 14-33 0-60-27-60-60z" />
+      <path d="M300 188h28v136h-28z" />
+      <path d="M348 188h28v136h-28z" opacity="0.9" />
+    </g>
   </svg>
 )
 
