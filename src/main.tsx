eabcc6e2670588ea0bee9782883ed2eb6afac861
samuel <masday@localhost.localdomain>
2026-01-25 11:32:20 -0300

    feat: add google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-99L062322T");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 createRoot(document.getElementById("root")!).render(<App />);
